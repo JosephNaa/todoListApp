5decc78794c23b4904d848d76e3cdc98b2c07bb3
JosephNaa <js.pekah@gmail.com>
2021-10-12 23:25:37 +0900

    initial commit

---
+++ todo-server/src/main/kotlin/js/pekah/todoserver/TodoServerApplication.kt
@@ -0,0 +1,11 @@
+package js.pekah.todoserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoServerApplication>(*args)
+}
