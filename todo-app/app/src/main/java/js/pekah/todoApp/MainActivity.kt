a2866e7d03e971fdc992c6969a1f305a2c15f2bb
JosephNaa <js.pekah@gmail.com>
2021-10-24 15:24:38 +0900

    initial commit

---
+++ todo-app/app/src/main/java/js/pekah/todoApp/MainActivity.kt
@@ -0,0 +1,11 @@
+package js.pekah.todoApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
